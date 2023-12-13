1f68b6363a05a5f35550069ab948d7fb6b4a77fb
Marco Upia <markremembers@gmail.com>
2023-12-13 11:59:41 -0500

    code

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import re
 
+import re
 class MyString:
 
     def __init__(self,value='') -> None:
@@ -18,47 +18,51 @@ class MyString:
             print("The value must be a string.")
 
     def is_sentence(self):
-        last_character = self.value[len(self.value)-1]
-        return last_character == "."
-        # self.value.endswith(".")
+        # last_character = self.value[-1]
+        # return self.value[-1] == "."
+        return self.value.endswith('.')
     
     def is_question(self):
-        last_character = self.value[len(self.value)-1]
-        return last_character == "?"
-        # self.value.endswith(".")
-
+        return self.value.endswith('?')
+    
     def is_exclamation(self):
-        last_character = self.value[len(self.value)-1]
-        return last_character == "!"
-        # self.value.endswith(".")
-
-
+        return self.value.endswith('!')
+    
     def count_sentences(self):
+        '''
+        "This is a string! It has three sentences. Right?"
+        --> 3
+        "a. b..."
+        --> 2
+        "a.b..c".split('.')
+        ['a','b','','c']
+        '''
         # re solution
-        # sentence_list = re.split('[?!.]', self.value)
-        # print(sentence_list)
-        # return len([sentence for sentence in sentence_list if sentence])
-
-        # replace/split
-        transformed_sentence = self.value.replace('!','.').replace('?', '.')
-        print(transformed_sentence)
-        sentence_list = transformed_sentence.split('.')
+        sentence_list = re.split('[?!.]', self.value)
         print(sentence_list)
-        actual_sentences = []
-        for sentence in sentence_list:
-            if sentence != '':
-                actual_sentences.append(sentence)
-        return len(actual_sentences)
+        return len([sentence for sentence in sentence_list if sentence])
 
+        # replace/split solution
+        # transformed_sentence = self.value.replace('!','.').replace('?','.')
+        # print(transformed_sentence)
+        # sentence_list = transformed_sentence.split('.')
+        # print(sentence_list)
+        # actual_sentences = []
+        # for sentence in sentence_list:
+        #     if sentence != '':
+        #         actual_sentences.append(sentence)
         # actual_sentences = [sentence for sentence in sentence_list if sentence != '']
-        
-        ## counting solution
-       
-            
+        # return len(actual_sentences)
+        # counting solution
+        # count = 0
+        # prev_char = ''
+        # for char in self.value:
+        #     if char in ['.','?','!'] and prev_char.isalpha():
+        #         count += 1
+        #     prev_char = char
+        # return count
 
-        
-  
 
-
-
-string = MyString('This, well, is a sentence. This')
+string = MyString('Hello World...')
+import ipdb; ipdb.set_trace()
+print(string.count_sentences())
